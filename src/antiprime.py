e53ae89991a632c0b22c10545818cc0ce17337a6
JC-therea <josemontanyesbq@gmail.com>
2024-10-17 16:37:27 +0200

    1st try

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,24 +1,47 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+def main(inputVal) :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
+	intInput = int(inputVal)
+
+	divi = 0
+	loop = 1
+	while loop <= intInput:
+		if intInput % loop == 0:
+			divi = divi + 1
+		loop = loop + 1
+
+	minorValues = 1
+	smallerDivi = 0
+
+	while minorValues < intInput:
+		smallerDiviLocal = 0
+		loop = 1
+		while loop <= minorValues:
+			if minorValues % loop == 0:
+				smallerDiviLocal = smallerDiviLocal + 1
+			if smallerDiviLocal > smallerDivi:
+				smallerDivi = smallerDiviLocal
+			loop = loop + 1
 
 
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
-	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+		minorValues = minorValues + 1
+
+	if smallerDivi < divi:
+		return("anti-prime")
+	else:
+		return("not anti-prime")
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+	import sys
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
+	## SOUGHT DINUCLEOTIDE.
+	print(main(sys.argv[1]))
